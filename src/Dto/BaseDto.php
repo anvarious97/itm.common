001ce105f0054217aedbf9ffad5721cc8fdef457
Anvarious97 <anvar_97@rambler.ru>
2026-01-13 12:08:19 +0600

    feat - BaseDto implements Stringable, JsonSerializable

--- src/Dto/BaseDto.php
+++ src/Dto/BaseDto.php
@@ -3,10 +3,13 @@
 namespace ITMobile\ITMobileCommon\Dto;
 
 use ITMobile\ITMobileCommon\Helpers\CamelCaseDataTransferObject;
+use JsonSerializable;
 use Spatie\DataTransferObject\DataTransferObject;
 use Spatie\DataTransferObject\Exceptions\UnknownProperties;
+use Stringable;
+use Throwable;
 
-abstract class BaseDto extends DataTransferObject
+abstract class BaseDto extends DataTransferObject implements Stringable, JsonSerializable
 {
     use CamelCaseDataTransferObject;
 
@@ -16,5 +19,19 @@ abstract class BaseDto extends DataTransferObject
     public static function fromArray(array $data): static
     {
         return new static(...$data);
+    }
+
+    public function __toString(): string
+    {
+        try {
+            return json_encode($this->toArray(), JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE) ?: '';
+        } catch (Throwable) {
+            return '';
+        }
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
     }
 }

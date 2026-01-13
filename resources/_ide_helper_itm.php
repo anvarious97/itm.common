d512f1203b7cb5852b75a1fea09c730d95542e04
anvarious97 <92132827+anvarious97@users.noreply.github.com>
2026-01-13 03:56:23 +0000

    Fix styling

--- resources/_ide_helper_itm.php
+++ resources/_ide_helper_itm.php
@@ -1,10 +1,12 @@
 <?php
+
 /** @noinspection AutoloadingIssuesInspection */
 
 namespace Illuminate\Database\Eloquent;
 
 /**
  * @method \ITMobile\ITMobileCommon\Pagination\ApiPaginator apiPaginate
+ *
  * @see \ITMobile\ITMobileCommon\Pagination\PaginationServiceProvider
  */
 class Builder {}

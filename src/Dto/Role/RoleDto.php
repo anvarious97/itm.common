7eb1079d1e1207cb68da4596f99e55c59817c704
Anvarious97 <anvar_97@rambler.ru>
2026-01-21 12:54:15 +0600

    fix(iam)

--- src/Dto/Role/RoleDto.php
+++ src/Dto/Role/RoleDto.php
@@ -6,17 +6,13 @@ use ITMobile\ITMobileCommon\Dto\BaseDto;
 use ITMobile\ITMobileCommon\Helpers\CamelCaseDataTransferObject;
 
 /**
- * @property int $id
  * @property string $name
- * @property string|null $guardName
  * @property array $permissions
  */
 class RoleDto extends BaseDto
 {
     use CamelCaseDataTransferObject;
 
-    public string $id;
     public string $name;
-    public ?string $guardName = null;
     public array $permissions = [];
 }

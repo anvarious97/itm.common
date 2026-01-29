7390f4d97161b318a2a96c08d29002a3cd2b32b9
anvarious97 <92132827+anvarious97@users.noreply.github.com>
2026-01-29 06:58:58 +0000

    Fix styling

--- src/Dto/User/UserListFilterDto.php
+++ src/Dto/User/UserListFilterDto.php
@@ -14,14 +14,21 @@ class UserListFilterDto extends BaseDto
     use CamelCaseDataTransferObject, TracksProvidedFields;
 
     public ?string $id;
+
     public ?string $companyId;
+
     public ?string $name;
+
     public ?string $email;
+
     public ?string $phone;
+
     public ?string $role;
+
     #[CastWith(EnumCaster::class, UserStatus::class)]
     public ?UserStatus $status;
 
     public ?string $sortBy = 'created_at';
+
     public ?string $sortDir = 'desc';
 }

549effbf8f4fd023994d27ca797a648298b4a894
Anvarious97 <anvar_97@rambler.ru>
2026-02-10 12:27:31 +0600

    feat(AuthenticatedUserDto) - added $relatedCompanyIds

--- src/Dto/User/AuthenticatedUserDto.php
+++ src/Dto/User/AuthenticatedUserDto.php
@@ -8,6 +8,7 @@ use ITMobile\ITMobileCommon\Dto\Traits\CamelCaseDataTransferObject;
 /**
  * @property string $userId UUID
  * @property string|null $companyId UUID
+ * @property array $relatedCompanyIds UUIDs
  * @property array $roles
  * @property array $permissions
  * @property string|null $impersonatorId UUID
@@ -19,6 +20,8 @@ class AuthenticatedUserDto extends BaseDto
     public string $userId;
 
     public ?string $companyId;
+
+    public array $relatedCompanyIds = [];
 
     public array $roles;
 
